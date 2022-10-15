40a03b446d5f0fe14ae9d33b4d0c2316243afc7d
Yakovlev Dima <zif18@rambler.ru>
2022-10-15 13:50:04 +0500

    add Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine ("Первое число: " + numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine ("Второе число: " + numberB);
+int result = numberA + numberB;
+Console.WriteLine("Сумма равна: " + result);
